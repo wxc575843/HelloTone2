5128666a49c9d2a346d47d116e0bcd48c41e48a1
wxc575843 <1040142133@qq.com>
2016-08-19 16:07:17 +0800

    init

---
+++ app/src/main/java/com/example/wxc575843/hellotone/main/BaseFragment.java
@@ -0,0 +1,44 @@
+package com.example.wxc575843.hellotone.main;
+
+/**
+ * Created by wxc575843 on 8/19/16.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.wxc575843.hellotone.R;
+
+
+public class BaseFragment extends Fragment {
+    private String title;
+    private int iconId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIconId() {
+        return iconId;
+    }
+
+    public void setIconId(int iconId) {
+        this.iconId = iconId;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, null, false);
+        TextView textView = (TextView) view.findViewById(R.id.text);
+        textView.setText(getTitle());
+        return view;
+    }
+}
